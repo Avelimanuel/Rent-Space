a556f96abaa5d3ceaae9453dd46499ad6eca0d24
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-10-21 18:42:33 +0300

    edited the property card

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -5,7 +5,7 @@ const PropertiesPage = () => {
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.length === 0 ? (<h1>No Properties Found</h1> ):(
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {properties.map((property)=>(
               <div> <PropertyCard key={property._id} property={property}/> </div>
             ))}
