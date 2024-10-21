21669c60442ffa30a9d4f1bf9c804f737d4c2210
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-10-21 12:30:38 +0300

    added the property card

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,5 +1,19 @@
-const InfoBoxes = () => {
-    return ( <div>Info Box</div> );
-}
- 
-export default InfoBoxes;
+import Link from 'next/link'
+const InfoBox = ({heading,children,buttonInfo,backgroundColor}) => {
+  return (
+    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold">{heading}</h2>
+      <p className="mt-2 mb-4">
+        {children}
+      </p>
+      <Link
+        href={buttonInfo.link}
+        className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+      >
+        {buttonInfo.text}
+      </Link>
+    </div>
+  );
+};
+
+export default InfoBox;
---
+++ components/PropertyCard.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FaBed,FaBath,FaRulerCombined,FaMoneyBill,FaMapMarker } from "react-icons/fa";
+
+const PropertyCard = ({ property }) => {
+  return <div></div>;
+};
+
+export default PropertyCard;
