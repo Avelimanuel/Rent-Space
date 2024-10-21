c891562d94e73c344098ea1932f77ae71559fde1
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-10-21 19:13:52 +0300

    added home properties

---
+++ components/HomeProperties.jsx
@@ -0,0 +1,39 @@
+import properties from "@/properties.json";
+import PropertyCard from "./PropertyCard";
+import Link from "next/link";
+
+const HomeProperties = () => {
+  const hotProperties = properties.slice(0, 3);
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-red-500 mb-6 text-center">
+            Hot properties
+          </h2>
+          {properties.length === 0 ? (
+            <h1>No Properties Found</h1>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {hotProperties.map((property) => (
+                <div>
+                  <PropertyCard key={property._id} property={property} />{" "}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+      <section className="m-auto max-w-lg px-6 my-10">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default HomeProperties;
