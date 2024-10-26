e49d440cdaa6bf94b01f50ecb906cc58703fa140
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-10-26 10:04:26 +0300

    added a single property details page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,10 +1,32 @@
+import Property from "@/models/Property";
+import connectToDb from "@/config/databaseconnect";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
-const SinglePropertyPage = ({params}) => {
-  
+const SinglePropertyPage = async ({ params }) => {
+  await connectToDb();
+  const singleProperty = await Property.findById(params.id).lean();
+
   return (
-    <div>
-      <h1>This is property number: {params.id}</h1>
-    </div>
+    <>
+      <PropertyHeaderImage image={singleProperty.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+        </div>
+      </section>
+    </>
   );
 };
 
