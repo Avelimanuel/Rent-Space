7ebc88d7b106b7348440bc0281946c93b5ae3842
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-11-04 06:59:37 +0300

    next-auth

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -11,7 +11,7 @@ const PropertiesPage = async () => {
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.length === 0 ? (
-          <h1>No Properties Found</h1>
+          <h1 className ="tetx-red">No Properties Found</h1>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {properties.map((property) => (
--- components/PropertyInfo.jsx
+++ components/PropertyInfo.jsx
@@ -92,7 +92,7 @@ const PropertyInfo = ({ property }) => {
             <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
               {property.amenities.map((amenity, index) => (
                 <li key={index}>
-                  <FaCheck className="inline-block"/>
+                  <FaCheck className="inline-block text-green-600 mr-2"/>
                   {amenity}
                 </li>
               ))}
