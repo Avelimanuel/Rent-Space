85fe615748785a06f6dabc105cfa497ec07e2ad0
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-11-24 17:12:03 +0300

    added the edit property form

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,8 +1,25 @@
-const EditPropertyPage = () => {
+import EditPropertyForm from "@/components/EditPropertyForm";
+import connectToDb from "@/config/databaseconnect";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/app/utils/convertToObject";
+
+const EditPropertyPage = async ({ params }) => {
+  await connectToDb();
+  const propertyDoc = await Property.findById(params.id).lean();
+  const property = convertToSerializableObject(propertyDoc);
+  if (!property) {
+    <h1 className="text-center font-bold text-2xl mt-10">
+      Property not found
+    </h1>;
+  }
   return (
-    <div>
-      <h1>Edit Property</h1>
-    </div>
+    <section className="bg-blue-50">
+      <div className="container py-24 max-w-2xl m-auto">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <EditPropertyForm property={property}/> 
+        </div>
+      </div>
+    </section>
   );
 };
 
